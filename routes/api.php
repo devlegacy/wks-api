686e25bf1aaf03837cda833812f4dd7ca0ba64a6
Samuel R <samuel_-_rojas@hotmail.com>
2019-05-28 02:22:59 -0500

    Test routes and update packages

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('welcome', function () {
+    return response()->json(['status' => 200, 'data' => 'Bienvenidos al Workshop de Laravel']);
+});
